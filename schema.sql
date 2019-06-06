3c7d05e9f5939bcca7f71c1ae8e8c8366398d39d
Kenneth Liu <ken.liu@gmail.com>
2019-06-06 10:52:52 -0400

    email should be not null

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 USE peeracks;
 CREATE TABLE IF NOT EXISTS acks (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    sender_email STRING,
+    sender_email STRING NOT NULL,
     msg STRING NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL
