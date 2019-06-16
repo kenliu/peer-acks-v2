fa1d34b87ada1c95521d73fee8aa3cb0ca7d01b0
Kenneth Liu <ken.liu@gmail.com>
2019-06-16 11:47:50 -0400

    move user email detection code to separate file

--- main.go
+++ main.go
@@ -175,30 +175,6 @@ func bindStaticRoutes(router *gin.Engine) {
 	router.LoadHTMLGlob("templates/*")
 }
 
-// GCP IAP sets this header for logged in users
-const GoogleIapUserHeader = "x-goog-authenticated-user-email"
-
-// email address only used for local testing (not running in IAP environment)
-const DevEmailAddress = "dev.email@cockroachlabs.com"
-
-func getUserEmail(c *gin.Context) string {
-	//log.Println("called getUserEmail()")
-	var email string
-
-	//check to see if we're running in a local environment and set a dummy user email
-	if os.Getenv("ENVIRONMENT") == "development" {
-		email = DevEmailAddress
-	} else if c.GetHeader(GoogleIapUserHeader) != "" {
-		email = c.GetHeader(GoogleIapUserHeader)
-		log.Println("detected logged in email header: " + email)
-		email = strings.ReplaceAll(email, "accounts.google.com:", "")
-		log.Println("detected logged in email: " + email)
-	} else {
-		log.Fatal("user email address not detected. app configuration problem?")
-	}
-	return email
-}
-
 // empty senderEmail string queries for all acks
 func fetchAcks(db *sql.DB, senderEmail string) []string {
 	log.Println("called fetchAcks()")
